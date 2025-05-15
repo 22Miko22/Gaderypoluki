ee7fb5b18564e5a16b2e87540f6d0429e94c261a
22Miko22 <daniil.pushkarov@teb.edu.pl>
2025-05-15 10:59:38 +0200

    Update Cypher.cs

--- Cypher.cs
+++ Cypher.cs
@@ -10,26 +10,25 @@ namespace Gaderypoluki
     {
         public static string Gaderypoluki(string text)
         {
-            //zamieniamy na duże litery i usuwamy spacje - chainowanie metod
+            
             text = text.ToUpper().Replace(" ", "");
             char[] charArray = text.ToCharArray();
             string output = String.Empty;
             foreach (char c in charArray)
             {
-                //jesli wpisano literę pomiedzy A a Z to jej wartośc liczbowa wynosi pomiędzy 65 a 90
-                //wyciągamy wartość liczbową poprzez jawne rzutowanie na int
+                
                 int charValue = (int)c;
-                //dodajemy wartość "klucza" - 3
+                
                 charValue += 3;
-                //sprawdzamy czy wynik mieści się w zakresie dużych liter alfabetu łacińskiego
+                
                 if (charValue > 90)
                 {
-                    //jeśli nie to odejmujemy 26 żeby "przewinąć do początku alfabetu"
+                    
                     charValue -= 26;
                 }
-                //konwertujemy wartość liczbową na znak
+                
                 char result = (char)charValue;
-                //dodajemy znak do wyniku
+                
                 output += result;
             }
             return output;
