e64b3f04655f2e2ead03ed1f10af8ae053a11d70
22Miko22 <daniil.pushkarov@teb.edu.pl>
2025-05-19 20:43:34 +0200

    Update Cypher.cs

--- Cypher.cs
+++ Cypher.cs
@@ -6,69 +6,55 @@ using System.Threading.Tasks;
 
 namespace Szyfry
 {
-     class Cypher
+    class Cypher
     {
+        
         public static string Gaderypoluki(string text)
         {
-            
-            text = text.ToUpper().Replace(" ", "");
-            char[] charArray = text.ToCharArray();
-            string output = String.Empty;
-            foreach (char c in charArray)
+            Dictionary<char, char> pairs = new Dictionary<char, char>()
             {
-                
-                int charValue = (int)c;
-                
-                charValue += 3;
-                
-                if (charValue > 90)
-                {
-                    
-                    charValue -= 26;
-                }
-                
-                char result = (char)charValue;
-                
-                output += result;
+                {'G', 'A'}, {'A', 'G'},
+                {'D', 'E'}, {'E', 'D'},
+                {'R', 'Y'}, {'Y', 'R'},
+                {'P', 'O'}, {'O', 'P'},
+                {'L', 'U'}, {'U', 'L'},
+                {'K', 'I'}, {'I', 'K'}
+            };
+
+            text = text.ToUpper().Replace(" ", "");
+            string output = "";
+
+            foreach (char c in text)
+            {
+                if (pairs.ContainsKey(c))
+                    output += pairs[c]; 
+                else
+                    output += c;        
             }
+
             return output;
         }
-        public static string DictionaryCaesar(string text)
+
+        public static string CaesarShift(string text)
         {
-            Dictionary<char, char> dict = new Dictionary<char, char>();
-            dict.Add('A', 'D');
-            dict.Add('B', 'E');
-            dict.Add('C', 'F');
-            dict.Add('D', 'E');
-            dict.Add('E', 'H');
-            dict.Add('F', 'I');
-            dict.Add('G', 'A');
-            dict.Add('H', 'K');
-            dict.Add('I', 'L');
-            dict.Add('J', 'M');
-            dict.Add('K', 'I');
-            dict.Add('L', 'U');
-            dict.Add('M', 'P');
-            dict.Add('N', 'Q');
-            dict.Add('O', 'R');
-            dict.Add('P', 'O');
-            dict.Add('Q', 'T');
-            dict.Add('R', 'Y');
-            dict.Add('S', 'V');
-            dict.Add('T', 'W');
-            dict.Add('U', 'X');
-            dict.Add('V', 'Y');
-            dict.Add('W', 'Z');
-            dict.Add('X', 'A');
-            dict.Add('Y', 'B');
-            dict.Add('Z', 'C');
             text = text.ToUpper().Replace(" ", "");
-            char[] charArray = text.ToCharArray();
-            string output = String.Empty;
-            foreach (char c in charArray)
+            string output = "";
+
+            foreach (char c in text)
             {
-                output += dict[c];
+                if (c >= 'A' && c <= 'Z')
+                {
+                    char shifted = (char)(c + 3);
+                    if (shifted > 'Z')
+                        shifted = (char)(shifted - 26);
+                    output += shifted;
+                }
+                else
+                {
+                    output += c;
+                }
             }
+
             return output;
         }
     }
